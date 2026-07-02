fc860e8ffe2eb0d137071b236e12123960832da5
790351279-stack <790351279@qq.com>
2026-07-02 16:38:53 +0800

    v7: network-first cache strategy

--- sw.js
+++ sw.js
@@ -1,15 +1,23 @@
-const CACHE_NAME = 'morning-workout-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+var CACHE_NAME = 'morning-workout-v7';
+var ASSETS = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(ASSETS); }));
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).then(function(response) {
+      var clone = response.clone();
+      caches.open(CACHE_NAME).then(function(c) { c.put(e.request, clone); });
+      return response;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) { return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); })); }));
 });
