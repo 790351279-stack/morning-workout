a38cd9d28a70d236d65e63655d8cd76c09ee760e
790351279-stack <790351279@qq.com>
2026-07-03 10:35:43 +0800

    v13: bump cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'morning-workout-v12';
+var CACHE_NAME = 'morning-workout-v13';
 var ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', function(e) {
